747998c93245b1c0c7ba356c45ec9f0ed845228f
Sayan Mondal <sayanm816@gmail.com>
2022-01-17 22:54:56 +0530

    update for production

--- app.js
+++ app.js
@@ -20,9 +20,9 @@ app.use(cors({ credentials: true, origin: true }));
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
-app.set("trust proxy", 1);
+app.enable("trust proxy");
 // cookies and file middleare
-app.use(cookieParser(process.env.JWT_SECRET));
+app.use(cookieParser());
 app.use(
   fileUpload({
     useTempFiles: true,
--- utils/cookieToken.js
+++ utils/cookieToken.js
@@ -6,6 +6,7 @@ const cookieToken = (user, res) => {
       Date.now() + process.env.COOKIE_TIME * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
+    secure: req.secure || req.headers("x-forwarded-proto") === "https",
   };
 
   user.password = undefined;
