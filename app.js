25433a6398667dbca7e042454073b905481afc20
Sayan Mondal <sayanm816@gmail.com>
2022-01-17 20:38:02 +0530

    middleware update

--- app.js
+++ app.js
@@ -18,8 +18,21 @@ app.use(cors({ credentials: true, origin: true }));
 
 // regular middleare
 app.use(express.json());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET,PUT,POST,DELETE,UPDATE,OPTIONS"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use(express.urlencoded({ extended: true }));
-app.set("trust proxy", 1);
+// app.set("trust proxy", 1);
 // cookies and file middleare
 app.use(cookieParser(process.env.JWT_SECRET));
 app.use(
