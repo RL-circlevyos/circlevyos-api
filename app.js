a520c129ce62e639fcc1497d07c54f22eb738295
Sayan Mondal <sayanm816@gmail.com>
2022-01-18 00:29:02 +0530

    update

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.enable("trust proxy");
 // cookies and file middleare
-app.use(cookieParser());
+app.use(cookieParser(process.env.JWT_SECRET));
 app.use(
   fileUpload({
     useTempFiles: true,
