fe98245f5b2780d7bdc181942ac82f8cf651876c
Sayan Mondal <sayanm816@gmail.com>
2022-01-18 15:15:14 +0530

    update cookie settings

--- utils/cookieToken.js
+++ utils/cookieToken.js
@@ -6,6 +6,9 @@ const cookieToken = (user, res) => {
       Date.now() + process.env.COOKIE_TIME * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    // signed: true,
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
   };
 
   user.password = undefined;
