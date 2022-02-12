d056331b62963217080b043d6da7beb08209dd40
Sayan Mondal <sayanm816@gmail.com>
2022-02-12 12:44:49 +0530

    logout update

--- controller/userController.js
+++ controller/userController.js
@@ -59,10 +59,13 @@ exports.login = BigPromise(async (req, res, next) => {
 
 exports.logout = BigPromise(async (req, res, next) => {
   res.cookie("token", null, {
+    expires: new Date(Date.now()),
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    // signed: true,
-    // sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+  });
+
+  res.status(200).json({
+    success: true,
+    message: "logout success",
   });
 });
 
