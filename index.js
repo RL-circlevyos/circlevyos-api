0719f2c1050ee73bd4dcf7d0b76d9d8f07ea0ee4
Sayan Mondal <sayanm816@gmail.com>
2022-02-12 21:36:19 +0530

    remove herku up

--- index.js
+++ index.js
@@ -26,29 +26,3 @@ process.on("SIGTERM", () => {
     console.log("process terminated");
   });
 });
-
-function startKeepAlive() {
-  setInterval(function () {
-    var options = {
-      host: "https://circlevyos-api-prod.herokuapp.com",
-      port: 80,
-      path: "/",
-    };
-    http
-      .get(options, function (res) {
-        res.on("data", function (chunk) {
-          try {
-            // optional logging... disable after it's working
-            console.log("HEROKU RESPONSE: " + chunk);
-          } catch (err) {
-            console.log(err.message);
-          }
-        });
-      })
-      .on("error", function (err) {
-        console.log("Error: " + err.message);
-      });
-  }, 20 * 60 * 1000); // load every 20 minutes
-}
-
-startKeepAlive();
